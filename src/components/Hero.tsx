03e387d11ca12110e38daf854786047bfbc14c42
ash <ashddev@proton.me>
2024-06-08 15:50:49 +0100

    we are gewd

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,9 @@ import Navbar from "./Navbar";
 const Hero = () => {
   return (
     <div className="bg-blue-950 min-h-screen">
-      <div className=" px-6 py-12">
+      <div className="px-6 py-12">
         <Navbar />
+        <div className="text-blue-100">hello world we are gewd</div>
       </div>
     </div>
   );
